437de6e08cc0886bd51fc22b404bc678ca9e1758
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-04-25 23:32:56 +0600

    Context API User Showed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,51 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../provider/UserProvider";
 
 const Header = () => {
+  const { user, logOut } = useContext(UserContext);
+
+  const handleSignout = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
   return (
     <>
       <div className="navbar bg-blue-200 rounded-xl">
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
         </div>
+
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
               <NavLink to="/">Home</NavLink>
             </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/register">Register</NavLink>
-            </li>
+            {user ? (
+              <li>
+                {" "}
+                <span>{user.email}</span>{" "}
+                <button
+                  onClick={handleSignout}
+                  className="btn btn-primary btn-outline"
+                >
+                  Sign Out
+                </button>{" "}
+              </li>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/login">Login</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/register">Register</NavLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
--- src/provider/UserProvider.jsx
+++ src/provider/UserProvider.jsx
@@ -1,32 +1,51 @@
-import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.init";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const UserContext = createContext(null);
 const auth = getAuth(app);
 // eslint-disable-next-line react/prop-types
-const UserProvider = ({children}) => {
-    const [user, setUser] = useState(null);
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
-    const loginUser = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password);
-    }
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut=()=>{
+    return signOut(auth);
+  }
 
-    const authInfo = {
-        user,
-        createUser,
-        loginUser,
-    }
-    return (
-        <div>
-            <UserContext.Provider value={authInfo}>
-                {children}
-            </UserContext.Provider>
-        </div>
-    );
+  // observe user state change
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("auth state changed", currentUser);
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    createUser,
+    loginUser,
+    logOut,
+  };
+  return (
+    <div>
+      <UserContext.Provider value={authInfo}>{children}</UserContext.Provider>
+    </div>
+  );
 };
 
 export default UserProvider;
